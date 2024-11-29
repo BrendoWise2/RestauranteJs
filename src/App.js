4390cd2c329f137e16ec10e185dc90c0e9eb90f8
JoLucas0 <thedgehog4@gmail.com>
2024-11-29 16:23:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import RestauranteList from './RestauranteList';
+import ClienteList from './ClienteList';
+import MesasList from './MesasList';
 
 
 function App() {
   return (
     <div className="App">
       <RestauranteList />
+      <ClienteList />
+      <MesasList />
     </div>
   );
 }
